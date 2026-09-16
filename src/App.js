25e5f408b92e54435afbf73895f9cc4f4c4ab33e
Karl Horky <karl.horky@gmail.com>
2026-09-16 10:12:20 +0200

    Remove period

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.
+          Learn React
         </a>
       </header>
     </div>
